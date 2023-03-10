d9e0115bdecfadb652301177d639d3c9ec62663e
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-03-10 14:26:54 -0600

    Removed flex and added padding

--- world-holidays/src/App.tsx
+++ world-holidays/src/App.tsx
@@ -2,19 +2,20 @@ import styled from "styled-components";
 
 function App() {
   return (
-    <div>
-      <Container>World Holidays</Container>
-    </div>
+    <Container>
+      <Title>World Holidays</Title>
+    </Container>
   );
 }
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 2rem;
+  padding: 1rem;
+`;
+
+const Title = styled.h1`
+  font-size: 1.5rem;
 `;
 
 export default App;
