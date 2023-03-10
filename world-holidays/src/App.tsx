d4b97e7f8848b08dca1d26078a6ffbf3bc29a787
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-03-10 14:19:34 -0600

    Installed and implemented basic style-components

--- world-holidays/src/App.tsx
+++ world-holidays/src/App.tsx
@@ -1,9 +1,20 @@
+import styled from "styled-components";
+
 function App() {
   return (
-    <div className="App">
-      <h1>World Holidays</h1>
+    <div>
+      <Container>World Holidays</Container>
     </div>
   );
 }
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2rem;
+`;
 
 export default App;
