610c9867a99bedbe623b7c06b5a0e3771a170c3c
Daniel Carlman <daniel.v.carlman@gmail.com>
2023-03-10 16:21:58 -0600

    Created a basic search component

--- world-holidays/src/App.tsx
+++ world-holidays/src/App.tsx
@@ -1,21 +1,39 @@
 import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
-    <Container>
-      <Title>World Holidays</Title>
-    </Container>
+    <>
+      <GlobalStyle />
+      <Container>
+        <Title>Holidays across the world</Title>
+        <SearchBar />
+      </Container>
+    </>
   );
 }
 
-const Container = styled.div`
-  width: 100%;
-  height: 100vh;
+const Container = styled.body`
   padding: 1rem;
+  background-color: #f0f0f0;
+  height: 100dvh;
 `;
 
 const Title = styled.h1`
   font-size: 1.5rem;
+`;
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  
+  body {
+    font-family: Arial, sans-serif;
+  } 
 `;
 
 export default App;
---
+++ world-holidays/src/components/SearchBar.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const SearchBar: React.FC = () => {
+  const [query, setQuery] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (query.trim()) {
+      setTimeout(() => {
+        console.log(query);
+      }, 1000);
+      setQuery("");
+    }
+    setQuery("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Container>
+        <Input
+          title="Numbers and symbols not allowed"
+          type="search"
+          pattern={`[A-Za-z ]+`}
+          value={query}
+          placeholder="Example: Brazil"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </Container>
+    </form>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 0.5rem;
+  gap: 0.5rem;
+`;
+
+const Input = styled.input`
+  border: 2px solid lightgray;
+  padding: 0.2rem;
+  outline: none;
+  color: #6366f1;
+  :focus {
+    border-color: #6366f1;
+  }
+  ::placeholder {
+    color: #333333;
+  }
+`;
+
+const Button = styled.button`
+  font-size: 1rem;
+  padding: 0.075rem 0.5rem;
+`;
+
+export default SearchBar;
