57a9a74bcbeaeb92d5a48f05942ffb8c4f381673
Sandrina Pereira <a.sandrina.p@gmail.com>
2023-05-01 22:33:35 +0100

    test(App): Add first test

---
+++ world-holidays/src/App.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+
+import App from "./App";
+
+describe("App", () => {
+  it("renders", () => {
+    render(<App />);
+
+    const title = screen.getByText("Vite + React");
+
+    expect(title).toBeInTheDocument();
+  });
+});
