5481f0725e91112999ca6eb2070acc552581418c
Daniel Carlman <54963071+danielcarlman@users.noreply.github.com>
2023-07-20 07:01:33 -0500

    Filter by type (#5)
    * Added message for invalid queries after 1st successful query
    
    * Fetches now use an API Key from localStorage
    
    * Searchbar: changed search to enter press
    
    * Moved API_KEY to .env
    
    * Created filter button
    
    * Included gap between button and button label
    
    * Put lobotomized owl in the correct component
    
    * Included checkboxes in popover
    
    * Stylized checkbox
    
    * feat: Add filtering functionality to list by type
    
    * Renamed Bla to Filter
    
    * Renamed PopOver to HolidayPopOverFilter
    
    * Changed Date format
    
    * Included Show All option
    
    * Created Show All button. Needs FIX to be default
    
    * Hacky solution to empty string issue with type
    
    * Renamed variable
    
    * Disabled Windows Focus Refetching
    
    * Fixed query error with enabled attribute and created Pill for colors
    
    * Changed color for holiday type based on type instead of primary_type and fixed unique key error
    
    * Shows current selection

--- src/App.tsx
+++ src/App.tsx
@@ -63,6 +63,7 @@ function Home() {
         <SearchBar onChange={handleOnChange} />
         <HolidayPopOverFilter
           defaultRadioValue={holidayType}
+          selected={holidayType}
           onRadioChange={handlePopOverChange}
         />
       </Filter>
